9c26666ac676b271165c6dacc1c59ab909d75f76
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2017-12-20 08:53:29 +0700

    define Email Department

--- src/com/nanda/apps/Email.java
+++ src/com/nanda/apps/Email.java
@@ -1,5 +1,7 @@
 package com.nanda.apps;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -12,5 +14,24 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("Email created: "+ this.firstName +" "+this.lastName);
+
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+    }
+
+    private String setDepartment(){
+        System.out.println("Department Codes\n1 for Sale\n2 for Development\n3 for Accounting\n0 for None\nEnter Department Codes:");
+        Scanner scanner = new Scanner(System.in);
+        int depChoice = scanner.nextInt();
+
+        if (depChoice == 1){
+            return "sales";
+        }else if (depChoice == 2){
+            return "development";
+        }else if (depChoice == 3){
+            return "accounting";
+        }else{
+            return "";
+        }
     }
 }
